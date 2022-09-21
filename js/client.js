ea53906e3e34a681f3e07c4892683f9f7a98152a
Subrata <adithadawn@gmail.com>
2022-09-21 18:52:38 +0530

    url change

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8800');
+const socket = io('https://chatappbackendindia.herokuapp.com');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
